2cfcbdc47b4d74a06f08f3c414f01db9a32d2526
spookto <52279309+spookto@users.noreply.github.com>
2026-03-05 06:42:17 +0300

    Add custom configuration for King Static

---
+++ custom.py
@@ -0,0 +1,14 @@
+# Generated using https://godot-build-options-generator.github.io
+
+extra_suffix = "king_static"
+production = "yes"
+module_camera_enabled = "no"
+module_enet_enabled = "no"
+module_mbedtls_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_openxr_enabled = "no"
+module_upnp_enabled = "no"
+module_webrtc_enabled = "no"
+module_websocket_enabled = "no"
+module_webxr_enabled = "no"
